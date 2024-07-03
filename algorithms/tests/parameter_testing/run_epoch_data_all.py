9390e2b5a4a9764c93cc53352f6accf014af1a3b
JayJehlicka <jindrich.jehlicka@mibcon.cz>
2024-07-03 20:43:30 +0200

    add 20 repeats

--- algorithms/tests/parameter_testing/run_epoch_data_all.py
+++ algorithms/tests/parameter_testing/run_epoch_data_all.py
@@ -4,14 +4,17 @@ import sys
 scripts = ['./ga_sa/epoch_data.py', './genetic/epoch_data.py', './simulated_annealing/epoch_data.py',
            './tabu_search/epoch_data.py']
 
-for script in scripts:
-    try:
-        print(f"Running {script}...")
-        # Run the script file
-        subprocess.run([sys.executable, script], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running {script}: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred while running {script}: {e}")
+repeat_count = 20
 
-print("All scripts have been processed.")
+for script in scripts:
+    for i in range(repeat_count):
+        try:
+            print(f"Running {script} (Iteration {i+1}/{repeat_count})...")
+            # Run the script file
+            subprocess.run([sys.executable, script], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred while running {script} (Iteration {i+1}): {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred while running {script} (Iteration {i+1}): {e}")
+
+print("We got all the epoch data!!!")
